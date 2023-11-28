b71502180d886c20e6625dbfa69027b665f33f65
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2023-11-28 12:01:17 +0100

    Update home

--- app/compose/Home/Home.js
+++ app/compose/Home/Home.js
@@ -1,7 +1,46 @@
 ﻿document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    // Implement form submission logic
-    // You may need to use Office.js APIs for interacting with Outlook
+    
+    // Forward the selected email using Office.js
+    Office.context.mailbox.item.getSelectedDataAsync(Office.MailboxEnums.ItemType.Message, function (result) {
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+            var selectedMessage = result.value;
+            
+            // Create a new email message for forwarding
+            var forwardingMessage = Office.context.mailbox.item.createForward();
+            
+            // Set the recipient email address
+            forwardingMessage.to.addRecipients([{ emailAddress: { address: 'jn@malling.no' } }]);
+            
+            // Set the email body
+            forwardingMessage.body.setAsync('ALFCRM', { coercionType: Office.CoercionType.Text }, function (bodyResult) {
+                if (bodyResult.status === Office.AsyncResultStatus.Succeeded) {
+                    // Send the forwarded email
+                    forwardingMessage.send(function (sendResult) {
+                        if (sendResult.status === Office.AsyncResultStatus.Succeeded) {
+                            // Forwarding was successful
+                            console.log('Email forwarded successfully.');
+                            
+                            // Continue with any additional form submission logic
+                            // ...
+                        } else {
+                            // Forwarding failed
+                            console.error('Error forwarding email: ' + sendResult.error.message);
+                        }
+                    });
+                } else {
+                    // Setting email body failed
+                    console.error('Error setting email body: ' + bodyResult.error.message);
+                }
+            });
+        } else {
+            // Getting selected email data failed
+            console.error('Error getting selected email data: ' + result.error.message);
+        }
+    });
+    
+    // Implement any remaining form submission logic here
+    // ...
 });
 
 // Change event listener for radio buttons to toggle the placeholder text
