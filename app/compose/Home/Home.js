eb6fcd1a5996717592fbb699f48f86b03a61d15e
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2023-11-28 12:19:15 +0100

    u

--- app/compose/Home/Home.js
+++ app/compose/Home/Home.js
@@ -4,52 +4,21 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     event.preventDefault();
 
     if (!submitted) { // Check if the form has not been submitted before
-        // Forward the selected email using Office.js
-        Office.context.mailbox.item.getSelectedDataAsync(Office.MailboxEnums.ItemType.Message, function (result) {
-            if (result.status === Office.AsyncResultStatus.Succeeded) {
-                var selectedMessage = result.value;
-                
-                // Create a new email message for forwarding
-                var forwardingMessage = Office.context.mailbox.item.createForward();
-                
-                // Set the recipient email address
-                forwardingMessage.to.addRecipients([{ emailAddress: { address: 'jn@malling.no' } }]);
-                
-                // Set the email body
-                forwardingMessage.body.setAsync('ALFCRM', { coercionType: Office.CoercionType.Text }, function (bodyResult) {
-                    if (bodyResult.status === Office.AsyncResultStatus.Succeeded) {
-                        // Send the forwarded email
-                        forwardingMessage.send(function (sendResult) {
-                            if (sendResult.status === Office.AsyncResultStatus.Succeeded) {
-                                // Forwarding was successful
-                                console.log('Email forwarded successfully.');
-                                
-                                // Change the text of the Submit button to "Takk!"
-                                var submitButton = document.getElementById('submitButton');
-                                submitButton.textContent = 'Takk!';
-                                
-                                submitted = true; // Set the flag to indicate submission
-                                
-                                // Disable the button after submission
-                                submitButton.setAttribute('disabled', 'disabled');
-                                
-                                // Continue with any additional form submission logic
-                                // ...
-                            } else {
-                                // Forwarding failed
-                                console.error('Error forwarding email: ' + sendResult.error.message);
-                            }
-                        });
-                    } else {
-                        // Setting email body failed
-                        console.error('Error setting email body: ' + bodyResult.error.message);
-                    }
-                });
-            } else {
-                // Getting selected email data failed
-                console.error('Error getting selected email data: ' + result.error.message);
-            }
-        });
+        // Change the text of the Submit button to "Takk!"
+        var submitButton = document.getElementById('submitButton');
+        submitButton.textContent = 'Takk!';
+        
+        submitted = true; // Set the flag to indicate submission
+        
+        // Disable the button after submission
+        submitButton.setAttribute('disabled', 'disabled');
+        
+        // Show the confirmation message
+        var confirmationMessage = document.getElementById('confirmationMessage');
+        confirmationMessage.style.display = 'block';
+        
+        // Implement any remaining form submission logic here
+        // ...
     }
 });
 
