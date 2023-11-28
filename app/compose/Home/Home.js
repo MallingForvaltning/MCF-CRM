d4a4e0cd39df84da79a02f0bed2885187d214a4a
MallingForvaltning <131775623+MallingForvaltning@users.noreply.github.com>
2023-11-28 09:44:49 +0100

    oppd4

--- app/compose/Home/Home.js
+++ app/compose/Home/Home.js
@@ -3,3 +3,18 @@
     // Implement form submission logic
     // You may need to use Office.js APIs for interacting with Outlook
 });
+
+// Change event listener for radio buttons to toggle the placeholder text
+document.querySelectorAll('input[type="radio"][name="role"]').forEach(radio => {
+    radio.addEventListener('change', function(event) {
+        var nameInput = document.getElementById('nameInput');
+        var ownerNotice = document.getElementById('ownerNotice');
+        if (event.target.value === 'leietaker') {
+            nameInput.placeholder = "Navn på leietaker..";
+            ownerNotice.style.display = 'none';
+        } else if (event.target.value === 'eiendomseier') {
+            nameInput.placeholder = "Navn på eiendom...";
+            ownerNotice.style.display = 'block';
+        }
+    });
+});
